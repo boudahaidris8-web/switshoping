7be6af9c7f8129035992b0aac68fa62feed13b0f
boudahaidris8-web <boudahaidris8@gmail.com>
2026-04-01 15:58:56 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,41 @@
+const products = [
+  {
+    name: "منتج 1",
+    price: 100,
+   image: "image/product1.jpg",
+   sizes: ["S", "M", "L", "XL"],
+   colors: ["NOIR", "BLANC"],
+        category: "clothing"
+  },
+  {
+    name: "منتج 2",
+    price: 150,
+   image: "image/product2.jpg",
+   sizes: ["S", "M", "L", "XL"],
+   colors: ["NOIR", "BLANC"],
+        category: "clothing"
+  },
+  {
+    name: "منتج3",
+    price: 550,
+   image: "image/product3.jpg",
+   sizes: ["40", "41", "42", "43"],
+        colors: ["NOIR"],
+  },{
+    name: "منتج 4",
+    price: 500,
+   image: "image/product4.jpg",
+   sizes: ["40", "41", "42", "43"],
+        colors: ["NOIR"],
+  },
+  {
+    name: "منتج 5",
+    price: 150,
+   image: "image/product5.jpg",
+   sizes: ["S", "M", "L", "XL"],
+   colors: ["NOIR", "BLANC"],
+        category: "clothing"
+  }
+  
+  
+];
